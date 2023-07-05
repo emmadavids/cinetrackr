f6965a0eb49a4d80e5e10880fa25f882ecff46b2
MMal87 <munibamalik@gmail.com>
2023-07-05 13:50:07 +0100

    working on web

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,7 +1,18 @@
+const movieSearch = require('./fetchfunctions.js');
+
 const MoviesController = {
-Index: (req, res) => {
-    res.render("movies/index", { title: "Cinetrackr" });
-}
-};
+Index: async (req, res) => {
+          try {
+            const year = req.query.year; // Get the year from the request query parameters
+            const movies = await searchMoviesByDateRange(year); // Fetch movies based on the year
+      
+            res.render("movies/index", { movies }); // Pass the movies data to the view
+          } catch (error) {
+            console.error(error);
+            res.status(500).send('Internal Server Error');
+          }
+        }
+      };
+      
 
 module.exports = MoviesController;
