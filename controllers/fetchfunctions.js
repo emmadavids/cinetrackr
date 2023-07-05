8790eee5b3b3f6445554aeb2ceb0fa1ca1c31585
emmadavids <davidsek88@gmail.com>
2023-07-05 16:01:16 +0100

    mergey

--- controllers/fetchfunctions.js
+++ controllers/fetchfunctions.js
@@ -1,11 +1,12 @@
-const API_KEY = require('../omdb_api_key')
+
 const API_KEY_T = require('../tmdb_api_key')
 
 const fetch = require('node-fetch');
 
+
 const currentYear = new Date().getFullYear();
 
-// search for movies by year range
+//search for movies by year range
 const searchMoviesByDateRange = (startDate = '', endDate = '') => {
   let url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY_T}&sort_by=popularity.desc`;
 
@@ -16,32 +17,6 @@ const searchMoviesByDateRange = (startDate = '', endDate = '') => {
   if (endDate) {
     url += `&primary_release_date.lte=${endDate}`;
   }
-git 
-  return fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      const movies = data.results;
-      // Process the movie results
-      console.log(movies);
-      return movies;
-    })
-    .catch(error => {
-      console.error(error);
-      return [];
-    });
-}
-
-const getLatestPopularMovies = () => {
-  // Get the current date
-  const currentDate = new Date().toISOString().split('T')[0];
-
-  // Calculate a recent date (e.g., 1 month ago)
-  const recentDate = new Date();
-  recentDate.setMonth(recentDate.getMonth() - 1);
-  const formattedRecentDate = recentDate.toISOString().split('T')[0];
-
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY_T}&sort_by=popularity.desc&primary_release_date.gte=${formattedRecentDate}&primary_release_date.lte=${currentDate}&media_type=movie`;
-
   return fetch(url)
     .then(response => response.json())
     .then(data => {
@@ -81,7 +56,7 @@ const getLatestMovies = () => {
 
 const searchMoviesByTitle = (title) => {
   const encodedTitle = encodeURIComponent(title);
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY_T}&query=${encodedTitle}&sort_by=popularity.desc&media_type=movie`;
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY_T}&query=${encodedTitle}&sort_by=popularity.desc&media_type=movie&sprt_by=popularity.desc`;
 
   return fetch(url)
     .then(response => response.json())
@@ -97,9 +72,43 @@ const searchMoviesByTitle = (title) => {
     });
   }
 
-  module.exports = {
-    searchMoviesByTitle,
-    getLatestMovies,
-    getLatestPopularMovies,
-    searchMoviesByDateRange,
-  };
+
+const getLatestPopularMovies = () => {
+  // Get the current date
+  const currentDate = new Date().toISOString().split('T')[0];
+
+  // Calculate a recent date (e.g., 1 month ago)
+  const recentDate = new Date();
+  recentDate.setMonth(recentDate.getMonth() - 1);
+  const formattedRecentDate = recentDate.toISOString().split('T')[0];
+
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY_T}&sort_by=popularity.desc&primary_release_date.gte=${formattedRecentDate}&primary_release_date.lte=${currentDate}&media_type=movie`;
+
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const movies = data.results;
+      // Process the movie results
+      console.log(movies);
+      return movies;
+    })
+    .catch(error => {
+      console.error(error);
+      return [];
+    });
+}
+
+
+module.exports = {
+  searchMoviesByTitle,
+  searchMoviesByDateRange,
+  getLatestMovies,
+  getLatestPopularMovies,
+};
+
+
+
+
+
+
+
---
+++ omdb_api_key.js
@@ -0,0 +1,1 @@
+module.exports = "10bee912"
