1966fad91f5dbe186004a48bb4d1f86984d8a14f
Alex Buzea <alex.buzea@outlook.com>
2023-07-06 15:16:39 +0100

    add watchlist to profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,7 +1,9 @@
 const ProfileController = {
     Index: (req, res) => {
-      res.render("profile/profile");
+        const watchList = req.session.user.watch_list; // Assuming the user's watch_list is stored in the session
+
+        res.render("profile/profile", { watch_list: watchList });
     },
-  };
+};
   
   module.exports = ProfileController;
