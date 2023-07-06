c8dd0bed06779170f19b109d57e873386add2589
ClaudiaGC1339 <claudiagonzalez1339@gmail.com>
2023-07-06 09:45:17 +0100

    separated out film index page and search page

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,21 +1,37 @@
-const fetchfunctions = require('./fetchfunctions')
+const fetchfunctions = require("./fetchfunctions");
 const MoviesController = {
     async Index(req, res) {
-    try {
-        const movies = await fetchfunctions.getLatestPopularMovies(); // Fetch the latest movies using the service function
-        res.render('movies/index', { movies }); // Render the view and pass the movies data
-    } catch (error) {
-        console.error(error);
-        res.render('movies/index', { movies: [] });}}, // Render the view with an empty movies array on error
-SearchByTitle: async (req, res) => {
-    try {
-        const title = req.body.title;
-        const movies = await fetchfunctions.searchMoviesByTitle(title);
+        try {
+            const movies = await fetchfunctions.getLatestPopularMovies(); // Fetch the latest movies using the service function
+            res.render("movies/index", { movies }); // Render the view and pass the movies data
+        } catch (error) {
+            console.error(error);
+            res.render("movies/index", { movies: [] });
+        }
+    }, // Render the view with an empty movies array on error
+    SearchByTitle: async (req, res) => {
+        try {
+            const title = req.body.title;
+            const movies = await fetchfunctions.searchMoviesByTitle(title);
 
-        res.render("movies/index", { movies });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal server error')
-    }
-}}
+            res.render("movies/search", { movies });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Internal server error");
+        }
+    },
+
+    SearchByGenre: async (req, res) => {
+        try {
+            const genre = req.body.genre;
+            const movies = await fetchfunctions.searchMoviesByGenre(genre);
+
+            res.render("movies/search", { movies });
+        } catch (error) {
+                console.error(error);
+                res.status(500).send("Internal server error");
+                }
+            }
+        };
+
 module.exports = MoviesController;
