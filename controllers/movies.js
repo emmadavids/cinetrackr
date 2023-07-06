37436369bec0d646842ee07c98b8e839437da307
ClaudiaGC1339 <claudiagonzalez1339@gmail.com>
2023-07-06 12:12:07 +0100

    added search by genre

--- controllers/movies.js
+++ controllers/movies.js
@@ -9,29 +9,89 @@ const MoviesController = {
             res.render("movies/index", { movies: [] });
         }
     }, // Render the view with an empty movies array on error
-    SearchByTitle: async (req, res) => {
+    // SearchByTitle: async (req, res) => {
+    //     try {
+    //         const title = req.body.title;
+    //         const movies = await fetchfunctions.searchMoviesByTitle(title);
+
+    //         res.render("movies/search", { movies });
+    //     } catch (error) {
+    //         console.error(error);
+    //         res.status(500).send("Internal server error");
+    //     }
+    // },
+
+    // SearchByGenre: async (req, res) => {
+    //     try {
+    //         const genre = req.body.genre;
+    //         const movies = await fetchfunctions.searchMoviesByGenre(genre);
+
+    //         res.render("movies/search", { movies });
+    //     } catch (error) {
+    //             console.error(error);
+    //             res.status(500).send("Internal server error");
+    //             }
+    //         }
+
+    SearchBy: async (req, res) => {
         try {
             const title = req.body.title;
-            const movies = await fetchfunctions.searchMoviesByTitle(title);
+            const releaseDate = req.body.release_date;
+            const genre = req.body.genre;
+            const movies = await fetchfunctions.searchMovies(title, releaseDate, genre);
 
             res.render("movies/search", { movies });
         } catch (error) {
             console.error(error);
             res.status(500).send("Internal server error");
         }
-    },
+    }
+};
 
-    SearchByGenre: async (req, res) => {
-        try {
-            const genre = req.body.genre;
-            const movies = await fetchfunctions.searchMoviesByGenre(genre);
 
-            res.render("movies/search", { movies });
-        } catch (error) {
-                console.error(error);
-                res.status(500).send("Internal server error");
-                }
-            }
-        };
+
+
 
 module.exports = MoviesController;
+
+
+
+
+
+// const fetchfunctions = require("./fetchfunctions");
+// const MoviesController = {
+//     async Index(req, res) {
+//         try {
+//             const movies = await fetchfunctions.getLatestPopularMovies(); // Fetch the latest movies using the service function
+//             res.render("movies/index", { movies }); // Render the view and pass the movies data
+//         } catch (error) {
+//             console.error(error);
+//             res.render("movies/index", { movies: [] });
+//         }
+//     }, // Render the view with an empty movies array on error
+//     SearchByTitle: async (req, res) => {
+//         try {
+//             const title = req.body.title;
+//             const movies = await fetchfunctions.searchMoviesByTitle(title);
+
+//             res.render("movies/search", { movies });
+//         } catch (error) {
+//             console.error(error);
+//             res.status(500).send("Internal server error");
+//         }
+//     },
+
+//     SearchByGenre: async (req, res) => {
+//         try {
+//             const genre = req.body.genre;
+//             const movies = await fetchfunctions.searchMoviesByGenre(genre);
+
+//             res.render("movies/search", { movies });
+//         } catch (error) {
+//                 console.error(error);
+//                 res.status(500).send("Internal server error");
+//                 }
+//             }
+//         };
+
+// module.exports = MoviesController;
--- routes/movies.js
+++ routes/movies.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const MoviesController = require("../controllers/movies");
 
 const sessionChecker = (req, res, next) => {
-	if (!req.session.user && !req.cookies.user_sid) {
-		res.redirect("/sessions/new");
-	} else {
-		next();
-	}
+    if (!req.session.user && !req.cookies.user_sid) {
+        res.redirect("/sessions/new");
+    } else {
+        next();
+    }
 };
 
 router.get("/", sessionChecker, MoviesController.Index);
-router.post("/search", sessionChecker, MoviesController.SearchByTitle);
-router.post("/search", sessionChecker, MoviesController.SearchByGenre);
+router.post("/search", sessionChecker, MoviesController.SearchBy);
+// router.post("/search", sessionChecker, MoviesController.SearchByGenre);
 
 
   
