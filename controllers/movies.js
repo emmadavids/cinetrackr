aefba881b23f1c4cb6017e1fc652db2cbf7c51f7
Muniba Malik <63554062+MMal87@users.noreply.github.com>
2023-07-07 10:20:23 +0100

    Revert "Date function"

--- controllers/movies.js
+++ controllers/movies.js
@@ -49,36 +49,22 @@ const MoviesController = {
 
 
 // Render the view with an empty movies array on error
-// SearchByTitle: async (req, res) => {
-    //     try {
-    //         const title = req.body.title;
-    //         const movies = await fetchfunctions.searchMoviesByTitle(title);
+SearchByTitle: async (req, res) => {
+    try {
+        const title = req.body.title;
+        const movies = await fetchfunctions.searchMoviesByTitle(title);
 
-    //         res.render("movies/search", { movies });
-    //     } catch (error) {
-    //         console.error(error);
-    //         res.status(500).send("Internal server error");
-    //     }
-    // },
+        res.render("movies/search", { movies });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal server error");
+    }
+},
 
-    // SearchByGenre: async (req, res) => {
-    //     try {
-    //         const genre = req.body.genre;
-    //         const movies = await fetchfunctions.searchMoviesByGenre(genre);
-
-    //         res.render("movies/search", { movies });
-    //     } catch (error) {
-    //             console.error(error);
-    //             res.status(500).send("Internal server error");
-    //             }
-    //         }
-
-    SearchBy: async (req, res) => {
+    SearchByGenre: async (req, res) => {
         try {
-            const title = req.body.title;
-            const releaseDate = req.body.release_date;
             const genre = req.body.genre;
-            const movies = await fetchfunctions.searchMovies(title, releaseDate, genre);
+            const movies = await fetchfunctions.searchMoviesByGenre(genre);
 
             res.render("movies/search", { movies });
         } catch (error) {
@@ -86,26 +72,6 @@ const MoviesController = {
             res.status(500).send("Internal server error");
         }
     }
-};
-
-    // SearchByGenre: async (req, res) => {
-    //     try {
-    //         const genre = req.body.genre;
-    //         const movies = await fetchfunctions.searchMoviesByGenre(genre);
-
-    //         res.render("movies/search", { movies });
-    //     } catch (error) {
-    //             console.error(error);
-    //             res.status(500).send("Internal server error");
-    //             }
-    //         }
-    //     };
-
-
-
-
+        };
 
 module.exports = MoviesController;
-
-
-
