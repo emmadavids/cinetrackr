e15a883b8f1b42311d158dcc2c5c07a0f2c76fa6
MMal87 <munibamalik@gmail.com>
2023-07-12 15:00:11 +0100

    beginner cypress tests

--- cypress/e2e/homepage_spec.cy.js
+++ cypress/e2e/homepage_spec.cy.js
@@ -7,6 +7,21 @@ describe('template spec', () => {
 describe("Log-in Page", () =>{
   it("has a title", () =>{
   cy.visit("/sessions/new");
-  cy.get("h1").should("contain", "Cinetrackr");
+  cy.get("h1").should("contain", "CineTrackr");
+  });
+});
+
+describe("Log-in Page - Sign-Up Button Check", () => {
+  it("has a link to sign-up", () => {
+    cy.visit("/sessions/new"); // Replace with the URL of your login page
+
+    cy.get('a[href="/users/new"]').should("be.visible");
+  });
+});
+
+describe("Log-in Page - Log-in Button Check", () => {
+  it("has a button for logging in", () => {
+    cy.visit("/sessions/new");
+  cy.get("input#submit").should("be.visible")
   });
 });
